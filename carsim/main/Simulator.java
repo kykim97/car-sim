21e3f21a76070fbd2f3327af93c1dbb2894dbb01
kykim97 <113568664+kykim97@users.noreply.github.com>
2022-12-20 15:41:05 +0900

    Update Simulator.java

--- carsim/main/Simulator.java
+++ carsim/main/Simulator.java
@@ -1,8 +1,22 @@
 package carsim.main;
 
 public class Simulator {
-
-    public static void main(String args[]) {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         
+        String modelName = args[0];
+        int zeroToHundred = 0;
+
+        System.out.println("Car Model is" + modelName);
+
+        if("BMW".equals(args[0])) {
+            zeroToHundred = 5;
+        } else if("Benz".equals(args[0])) {
+            zeroToHundred = 8;
+        }
+        for(int i = 0; i<10; i++) {
+            double speed = (100 / zeroToHundred) * i;
+
+            System.out.println("" + i + "sec.speed is" + speed);
+        }
     }
 }
